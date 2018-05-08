77a035daff355570f360a38dc50718665736c447
Bogdan Petre <bogdan.petre0510@gmail.com>
2018-05-08 16:53:16 +0200

    Write statistics to csv file

--- statistics.py
+++ statistics.py
@@ -1,5 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
+import datetime
+
+stars = ''
+downloads = ''
 
 req_github = requests.get('https://api.github.com/repos/flask-dashboard/Flask-MonitoringDashboard')
 
@@ -7,8 +12,8 @@ if req_github.status_code != 200:
     print('Request to Github failed with code %d' %(req_github.status_code))
 
 else:
-    print('Number of stars:')
-    print(req_github.json()['stargazers_count'])
+    stars = req_github.json()['stargazers_count']
+    print('Number of stars: %s' %(stars))
 
 
 req_pepy = requests.get('http://pepy.tech/count/Flask-MonitoringDashboard')
@@ -17,8 +22,13 @@ if req_pepy.status_code != 200:
 else:
     html_doc = req_pepy.text
     soup = BeautifulSoup(html_doc, 'html.parser')
-    print('Number of downloads:')
-    print(soup.body.div.div.p.b.string)
+    downloads = soup.body.div.div.p.b.string
+    print('Number of downloads: %s' %(downloads))
+
+timestamp = datetime.datetime.now()
+with open('stats.csv', 'a') as writeFile:
+    writer = csv.writer(writeFile)
+    writer.writerow([stars, downloads, timestamp.strftime("%Y-%m-%d %H:%M:%S")])
 
 
 
